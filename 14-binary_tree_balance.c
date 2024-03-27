a5c139520fc2851ab3a5b9bb86b96e329dec4d29
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-03-27 17:39:57 +0100

    changed the check to return 0 instead of 1

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,9 +11,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree != NULL)
 	{
 		size_t left_height = (tree->left) ?
-			(1 + binary_tree_height(tree->left)) : 0;
+			(1 + binary_tree_height(tree->left)) : 1;
 		size_t right_height = (tree->right) ?
-			(1 + binary_tree_height(tree->right)) : 0;
+			(1 + binary_tree_height(tree->right)) : 1;
 		return ((left_height > right_height) ?
 			left_height : right_height);
 	}
