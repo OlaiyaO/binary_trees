47ff3dc8df4c03f041161fb6258bf69897378065
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-03-27 17:13:02 +0100

    Still solving

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,10 +18,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
+	new_node = binary_tree_node(parent, value);
+
 	if (new_node == NULL)
 		return (NULL);
-
-	new_node = binary_tree_node(parent, value);
 
 	if (parent->left != NULL)
 	{
